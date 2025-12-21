69626245ed362f9385b49c3f34be2ff81200038b
Amin Shali <amshali@users.noreply.github.com>
2025-12-21 14:44:39 -0800

    fixed reset svg

--- ks/ks.js
+++ ks/ks.js
@@ -124,7 +124,7 @@ export class PlayerManager {
         </div>
         <div class="d-flex justify-content-end gap-2 mt-2">
             <button class="btn btn-sm btn-outline-warning d-flex align-items-center justify-content-center" style="width: 32px; height: 32px;" onclick="playerManager.resetScore('${id}')" title="Reset Score">
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2.5 2v6h6M2.5 8a10 10 0 1 1 2.36 5.06"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2.5 4v7h6M2.5 11a7 7 0 1 1 2.36 5.06"/></svg>
             </button>
             <button class="btn btn-sm btn-outline-danger" onclick="playerManager.removePlayer('${id}')">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
